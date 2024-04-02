9792ed98664fb82b994dbd2c4580bb32399fd80d
utkarsh <khareu550@gmail.com>
2024-04-02 19:46:26 +0530

    Update API endpoints

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,15 @@ const nextConfig = {
       return [
         {
           source: '/youth-parliament/new',
-          destination: `http://localhost:3001/youth-parliament/new/`,
+          destination: `http://13.50.142.239:3001/youth-parliament/new`,
         },
         {
           source: '/quiz/new',
-          destination: `http://localhost:3001/quiz/new/`,
+          destination: `http://13.50.142.239:3001/quiz/new/`,
         },
         {
           source: '/mock-test/new',
-          destination: `http://localhost:3001/mock-test/new/`,
+          destination: `http://13.50.142.239:3001/mock-test/new/`,
         },
       ];
     },
